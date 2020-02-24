727d7c528da838b4c7784200eb391285b6722814
sitinadiah25 <sitinadiah333@gmail.com>
2020-02-24 20:46:59 +0800

    Add cache versioning

--- sw.js
+++ sw.js
@@ -1,5 +1,7 @@
 //service worker : can add things to cache so it works offline 
-const staticCacheName = 'site-static';
+
+//update version every time changes are made in static files
+const staticCacheName = 'site-static-v2';
 const assets = [
     '/',
     'index.html',
@@ -13,9 +15,10 @@ const assets = [
     'https://fonts.gstatic.com/s/materialicons/v50/flUhRq6tzZclQEJ-Vdg-IuiaDsNcIhQ8tQ.woff2'
 ];
 
-//install service worker 
+//install service worker (only runs if service worker file is changed)
 self.addEventListener('install', evt => {
     // console.log('service worker has been installed');
+    // wait until makes sure install is still running while caches are being loaded
     evt.waitUntil(
         caches.open(staticCacheName).then(cache => {
             console.log('caching shell assets');
@@ -27,6 +30,17 @@ self.addEventListener('install', evt => {
 //activate event
 self.addEventListener('activate', evt => {
     // console.log('service worker has been activated');
+    evt.waitUntil(
+        caches.keys().then(keys => {
+            // console.log(keys);
+            //if all promises (keys) is resolved, then promise.all will resolve too
+            //keys is an array of promises
+            return Promise.all(keys
+                .filter(key => key !== staticCacheName) // caches want to be deleted stays in array 
+                .map(key => caches.delete(key)) // delete all the filtered caches
+            )
+        })
+    );
 });
 
 //fetch event 
